dc789b9da68922602c2d3a94b03233ba7893eb21
Kgotso Maduna <kgmaduna01@gmail.com>
2021-09-22 10:56:54 +0200

    modified

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class RegisterController extends Controller
+class LoginController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -82,39 +82,37 @@ class RegisterController extends Controller
         //
     }
 
-    public function register(Request $request)
+    
+public function login(Request $request)
 {
-    // Here the request is validated. The validator method is located
-    // inside the RegisterController, and makes sure the name, email
-    // password and password_confirmation fields are required.
-   // $this->validator($request->all())->validate();
-       
-    $this->validate($request, [ //inputs are not empty or null
-        'name' => 'required|string',
-        'email' => 'required|string',
-        'password' =>'required|string|confirm|min:6',
-    ]);
+    
+    $this->validateLogin($request);
 
-    // A Registered event is created and will trigger any relevant
-    // observers, such as sending a confirmation email or any 
-    // code that needs to be run as soon as the user is created.
-    event(new Registered($user = $this->create($request->all())));
 
-    // After the user is created, he's logged in.
-    $this->guard()->login($user);
 
-    // And finally this is the hook that we want. If there is no
-    // registered() method or it returns null, redirect him to
-    // some other URL. In our case, we just need to implement
-    // that method to return the correct response.
-    return $this->registered($request, $user)
-                    ?: redirect($this->redirectPath());
+    if ($this->attemptLogin($request)) {
+        $user = $this->guard()->user();
+        $user->generateToken();
+
+        return response()->json([
+            'data' => $user->toArray(),
+        ]);
+    }
+
+    return $this->sendFailedLoginResponse($request);
 }
 
-  protected function registered(Request $request, $user)
-  {
-    $user->generateToken();
+public function logout(Request $request)
+{
+    $user = Auth::guard('api')->user();
 
-    return response()->json(['data' => $user->toArray()], 201);
-  }
+    if ($user) {
+        $user->api_token = null;
+        $user->save();
+    }
+
+    return response()->json(['data' => 'User logged out.'], 200);
+}
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +21,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('tasks',TaskController::class);
+Route::middleware('auth:api')
+    ->get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::post('register','Auth\RegisterController@register');
+
+
+
+
+    Route::post('auth\register',[RegisterController::class,'register']);
+
+
+
+Route::resource('tasks',TaskController::class);
+// Route::post('register','Auth\RegisterController@register');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
